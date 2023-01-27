f51a9630ae6ba5c6273c4d7452daf4946c99a75b
abusayed22 <abusayed.in22@gmail.com>
2023-01-27 11:03:40 +0600

    redux concept inisitate

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const { createStore } = require("redux");
 
 // inisital state
-const inisitalState = [ ]
+const inisitalState = {
+    todo: []
+}
+
+// reducer
+const todoReducer = (state = inisitalState, aciton) => {
+    switch (aciton.type) {
+        case "todo/added":
+            return {
+                ...state,
+                todo: [
+                    ...state.todo,
+                    {title: aciton.payload}
+                ]
+            }
+
+        case "todo/loaded":
+            return {
+                ...state,
+                todo: [
+                    ...state.todo,
+                    {title: aciton.payload}
+                ]
+            }
+
+            return state;
+    }
+
+}
+
+
+// create store 
+const store = createStore(todoReducer);
+
+
+// subscribe for state change which provider in react
+store.subscribe(() => {
+    console.log(store.getState())
+    // getState =>  state value get for use getState()
+});
+
+// dispatch with value
+store.dispatch({
+    type: "todo/added",
+    payload: 'Learn redux'
+})
